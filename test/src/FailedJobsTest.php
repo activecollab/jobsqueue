8372874ed931f291d6db3a5d51f023c4c1de2d43
Ilija Studen <ilija.studen@activecollab.com>
2015-08-16 00:54:49 +0200

    Started working on failed jobs log

---
+++ test/src/FailedJobsTest.php
@@ -0,0 +1,12 @@
+<?php
+
+  namespace ActiveCollab\JobsQueue\Test;
+
+  use ActiveCollab\DatabaseConnection\Test\TestCase;
+
+  /**
+   * @package ActiveCollab\JobsQueue\Test
+   */
+  class FailedJobsTest extends TestCase
+  {
+  }

b29a9ccb4a8b963638406f9e9cbfc6ece5ac0269
Ilija Studen <ilija.studen@gmail.com>
2021-09-05 18:49:18 +0200

    Enable client apps to specify additional queue tables

--- src/Command/CreateTables.php
+++ src/Command/CreateTables.php
@@ -9,54 +9,54 @@
  * with this source code in the file LICENSE.
  */
 
+declare(strict_types=1);
+
 namespace ActiveCollab\JobsQueue\Command;
 
+use ActiveCollab\JobsQueue\Queue\MySqlQueue\AdditionalTablesResolverInterface;
 use Exception;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-/**
- * @package ActiveCollab\JobQueue\Command
- */
 class CreateTables extends Command
 {
-    /**
-     * Configure command.
-     */
     protected function configure()
     {
         parent::configure();
 
         $this->setName('create_tables')
-            ->setDescription('Create tables that are needed for MySQL queue to work');
+            ->setDescription('Create tables that are needed for MySQL queue to work')
+            ->addOption(
+                'additional-tables-resolver',
+                '',
+                InputOption::VALUE_REQUIRED,
+                'Container key where additional tables resolver is available.',
+                'additional_tables_resolver'
+            );
     }
 
-    /**
-     * @param  InputInterface  $input
-     * @param  OutputInterface $output
-     * @return int
-     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         try {
-            $this->dispatcher->getQueue()->createTables("CREATE TABLE IF NOT EXISTS `email_log` (
-              `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-              `instance_id` int(10) unsigned NOT NULL DEFAULT '0',
-              `parent_type` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-              `parent_id` int(10) unsigned DEFAULT NULL,
-              `sender` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-              `recipient` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-              `subject` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-              `message_id` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-              `sent_on` datetime DEFAULT NULL,
-              PRIMARY KEY (`id`),
-              KEY `message_id` (`message_id`),
-              KEY `instance_id` (`instance_id`)
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;");
+            $this->dispatcher->getQueue()->createTables(...$this->getAdditionalTables());
 
             return $this->success('Done', $input, $output);
         } catch (Exception $e) {
             return $this->abortDueToException($e, $input, $output);
         }
+    }
+
+    private function getAdditionalTables(): array
+    {
+        if ($this->getContainer()->has('additional-tables-resolver')) {
+            $additional_tables_resolver = $this->getContainer()->get('additional-tables-resolver');
+
+            if ($additional_tables_resolver instanceof AdditionalTablesResolverInterface) {
+                return $additional_tables_resolver->getAdditionalTables();
+            }
+        }
+
+        return [];
     }
 }

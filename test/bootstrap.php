89263b4926665b4178e52b3a0cfc2c03b2d06cb6
Ilija Studen <ilija.studen@gmail.com>
2021-09-18 09:59:33 +0200

    Simplify bootstrapper as we can rely on autoloader

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -9,15 +9,9 @@
  * with this source code in the file LICENSE.
  */
 
-/**
- * Bootstrap test case.
- */
 date_default_timezone_set('GMT');
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
 exec('mysql -u root -e "DROP DATABASE IF EXISTS activecollab_jobs_queue_test"');
 exec('mysql -u root -e "CREATE DATABASE activecollab_jobs_queue_test"');
-
-require __DIR__ . '/src/Base/TestCase.php';
-require __DIR__ . '/src/Base/IntegratedTestCase.php';
